3b64c57e873b4f1304cb3a4e9e16d20715b0f3be
Ramiro <raminp.1012@gmail.com>
2022-05-15 17:43:25 -0500

    Initial commit

---
+++ src/test/java/com/softrami/microservicio/zuul/MicroservicioZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrami.microservicio.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
